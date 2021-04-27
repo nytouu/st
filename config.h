128b98abc787ceab63b6d723fe29ea18411633f6
nytouu <capitaine7312@gmail.com>
2021-04-27 19:21:24 +0200

    opaque terminal

--- config.h
+++ config.h
@@ -107,7 +107,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 4;
 
 /* bg opacity */
-float alpha = 0.90;
+float alpha = 1;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
