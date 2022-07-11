6a04761876bbe418359909f946b85c03b24b845e
nytouu <capitaine7312@gmail.com>
2022-07-12 01:20:13 +0200

    alpha

--- config.h
+++ config.h
@@ -122,8 +122,8 @@ char *termname = "st-256color";
 unsigned int tabspaces = 4;
 
 /* bg opacity */
-/* float alpha = 1; */
-float alpha = 0.95;
+float alpha = 1;
+/* float alpha = 0.95; */
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
