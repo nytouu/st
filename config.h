f0a252dadae53d30d2d3c175aa486db73b78fdc7
nytouu <capitaine7312@gmail.com>
2022-11-18 02:56:57 +0100

    font size

--- config.h
+++ config.h
@@ -5,10 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "SF Mono:style=Regular:size=12";
+static char *font = "SF Mono:style=Regular:size=14";
 static char *font2[] = {
-	"nonicons:style=Regular:size=12",
-	"Liga SFMono Nerd Font:style=Regular:size=12",
+	"nonicons:style=Regular:size=14",
+	"Liga SFMono Nerd Font:style=Regular:size=14",
 };
 /* static char *font = "JetBrainsMono Nerd Font:style=Regular:size=12"; */
 /* static char *font2[] = { */
